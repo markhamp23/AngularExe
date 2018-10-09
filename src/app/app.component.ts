88701fe53c04226d401da788fffafe2310e1a80e
Patrick Markham <markhamp23@gmail.com>
2018-10-09 11:35:48 +0200

    Exemple herois

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'exercici';
+  title = 'exercici hola';
+
+  heros: string[] = ["Superman", "Batman", "Spiderman", "Ironman", "Hulk", "Magneto"];
+
+  herosObject: Hero[] = [];
+
+  recuperat: boolean = false;
+
+  constructor() {
+    //console.log(this.heros.toString());
+    this.herosObject.push(new Hero("Superman", 10, "Velocitat", 5000, 1930));
+    this.herosObject.push(new Hero("Batman", 10, "Velocitat", 5000, 1930));
+    this.herosObject.push(new Hero("Spiderman", 10, "Velocitat", 5000, 1930));
+    this.herosObject.push(new Hero("Ironman", 10, "Velocitat", 5000, 1930));
+    this.herosObject.push(new Hero("Hulk", 1000, "Força", 50, 1970));
+    this.herosObject.push(new Hero("Magneto", 10, "Velocitat", 5000, 1930));
+  }
+
+  recuperarDades(event) {
+    this.recuperat = !this.recuperat;
+  }
+
+
+}
+
+export class Hero {
+  private _nom: string;
+  private _forca: number;
+  private _poder: string;
+  private _velocitat: number;
+  private _any: number;
+
+
+  constructor(nom: string, forca: number, poder: string, velocitat: number, any: number) {
+    this._nom = nom;
+    this._forca = forca;
+    this._poder = poder;
+    this._velocitat = velocitat;
+    this._any = any;
+  }
+
+  get nom(): string {
+    return this._nom;
+  }
+
+  set nom(value: string) {
+    this._nom = value;
+  }
+
+  get forca(): number {
+    return this._forca;
+  }
+
+  set forca(value: number) {
+    this._forca = value;
+  }
+
+  get poder(): string {
+    return this._poder;
+  }
+
+  set poder(value: string) {
+    this._poder = value;
+  }
+
+  get velocitat(): number {
+    return this._velocitat;
+  }
+
+  set velocitat(value: number) {
+    this._velocitat = value;
+  }
+
+  get any(): number {
+    return this._any;
+  }
+
+  set any(value: number) {
+    this._any = value;
+  }
 }
