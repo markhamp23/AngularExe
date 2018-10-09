3518c295c7b90cd6bbcad48b5ec5c3d02b17ec34
Patrick Markham <markhamp23@gmail.com>
2018-10-09 12:27:24 +0200

    Exemple herois

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'exercici hola';
 
   heros: string[] = ["Superman", "Batman", "Spiderman", "Ironman", "Hulk", "Magneto"];
@@ -29,7 +29,85 @@ export class AppComponent {
   }
 
 
+
+  // PORTA
+  // 1.- Crea una classe Porta, aquesta ha de tenir els atributs de ample, alt, color i oberta (true si oberta i
+  // false si tancada). A aquesta porta hem de crear els mètodes de Obrir, Tancar i mostrar estat (mostra
+  // tots els atributs).
+  // 2.- Instanciar una porta, obrir-la i mostrar el seu estat.
+  // 3.- Crear els mètodes necessaris per a poder inicialitzar cadascuna de les variables i per a que ens
+  // retorne el valor de cada variable.
+
+
+  ngOnInit(): void {
+    let porta1: Porta = new Porta();
+    porta1.ample = 200;
+    porta1.oberta = false;
+    console.log("Inicial", porta1.estat());
+    porta1.obrir();
+    console.log("Final", porta1.estat());
+    porta1.tancar();
+    console.log("Final", porta1.estat());
+  }
+
 }
+
+export class Porta {
+  private _ample: number;
+  private _alt: number;
+  private _color: string;
+  private _oberta: boolean;
+
+
+  constructor() {}
+
+  obrir(): void {
+    this._oberta = true;
+  }
+
+  tancar(): void {
+    this._oberta = false;
+  }
+
+  estat(): string {
+    return "ample: " + this.ample + " oberta: " + this.oberta;
+  }
+
+  get ample(): number {
+    return this._ample;
+  }
+
+  set ample(value: number) {
+    this._ample = value;
+  }
+
+  get alt(): number {
+    return this._alt;
+  }
+
+  set alt(value: number) {
+    this._alt = value;
+  }
+
+  get color(): string {
+    return this._color;
+  }
+
+  set color(value: string) {
+    this._color = value;
+  }
+
+  get oberta(): boolean {
+    return this._oberta;
+  }
+
+  set oberta(value: boolean) {
+    this._oberta = value;
+  }
+}
+
+
+
 
 export class Hero {
   private _nom: string;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule, MatIconModule, MatMenuModule} from "@angular/material";
+import {PortaComponent} from "./porta/porta.component";
+import {PortaModule} from "./porta/porta.module";
+
+
+const appRoutes: Routes = [
+  { path: '', component: AppComponent},
+  { path: 'porta', loadChildren: './porta/porta.module#Portamodule'},
+];
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
     BrowserModule,
     MatCardModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    PortaModule
   ],
   exports: [
     MatCardModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    PortaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
